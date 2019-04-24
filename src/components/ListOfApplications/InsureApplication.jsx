ab4f41b2179ff29e59ba3220fcfc24dadd2ca11c
Marat <kashaevmt@gmail.com>
2019-04-24 23:39:37 +0300

    add request of applications

--- src/components/ListOfApplications/InsureApplication.jsx
+++ src/components/ListOfApplications/InsureApplication.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import { Button, Modal, Grid } from 'semantic-ui-react'
 
-const InsureApplicationModal = ({open, close, data}) => (
+const tmp = [
+  {
+    "insurancerequestid": 4388924262558877900,
+    "propertytype": "home1",
+    "amount": 10000,
+    "policystartdate": "2019-04-16",
+    "policyenddate": "2019-04-16",
+    "policycreatedcate": "2019-04-16",
+    "status": "pending",
+    "address": {
+        "house_num": "1223",
+        "apartment_num": "string",
+        "street": "string",
+        "city": "string",
+        "state": "string",
+        "country": "string"
+    },
+    "user": {
+        "first_name": "Nethmi",
+        "middle_name": "Thileka",
+        "last_name": "Hettiarachchi",
+        "email": "a2222saa@gmafil.com22",
+        "mobile_num": "01311158f9654455",
+        "passport_num": "12w111sf3dErf",
+        "passport_issued_by": "Sri Lanka",
+        "passport_issued_date": "2019-04-13"
+    },
+    "company": {
+        "companyid": 1,
+        "companyname": "ABC Insurance",
+        "address": {
+            "house_num": "No12124",
+            "apartment_num": "Dorm3",
+            "street": "1, Universrity st",
+            "city": "Innopolis",
+            "state": "Tatarstan",
+            "country": "Russia"
+        }
+    }
+  },
+]
+
+const InsureApplicationModal = ({open, close, data, handleOnProof}) => (
   <Modal open={open} onClose={close}>
-    {console.log(data)}
     <Modal.Header>Insurance contract number: {data===''? '' : data.numberOfApp}</Modal.Header>
     <Modal.Content>
       <Grid columns={4}>
@@ -20,15 +61,15 @@ const InsureApplicationModal = ({open, close, data}) => (
             <p>Date of application:</p>
           </Grid.Column>
           <Grid.Column>
-            <p>{data===''? '' : data.insureData.typeOfProperty}</p>
-            <p>{data===''? '' : data.insureData.city}</p>
-            <p>{data===''? '' : data.insureData.street}</p>
-            <p>{data===''? '' : data.insureData.houseNumber}</p>
-            <p>{data===''? '' : data.insureData.appartmentNumber}</p>
-            <p>{data===''? '' : data.insureData.cost}</p>
-            <p>{data===''? '' : data.insureData.dateStartOfAgreement}</p>
-            <p>{data===''? '' : data.insureData.dateEndOfTheAgreement}</p>
-            <p>{data===''? '' : data.insureData.dateOfApplication}</p>
+            <p>{data===''? '' : data.propertytype}</p>
+            <p>{data===''? '' : data.address.city}</p>
+            <p>{data===''? '' : data.address.street}</p>
+            <p>{data===''? '' : data.address.house_num}</p>
+            <p>{data===''? '' : data.address.apartment_num}</p>
+            <p>{data===''? '' : data.amount}</p>
+            <p>{data===''? '' : data.policystartdate}</p>
+            <p>{data===''? '' : data.policyenddate}</p>
+            <p>{data===''? '' : data.policycreatedcate}</p>
           </Grid.Column>
 
           <Grid.Column>
@@ -43,15 +84,15 @@ const InsureApplicationModal = ({open, close, data}) => (
             <p>Email:</p>
           </Grid.Column>
           <Grid.Column>
-            <p>{data===''? '' : data.personalData.name}</p>
-            <p>{data===''? '' : data.personalData.surname}</p>
-            <p>{data===''? '' : data.personalData.lastName}</p>
-            <p>{data===''? '' : data.personalData.passportSeria}</p>
-            <p>{data===''? '' : data.personalData.passportNumber}</p>
-            <p>{data===''? '' : data.personalData.passportIssueDate}</p>
-            <p>{data===''? '' : data.personalData.passportIssuedBy}</p>
-            <p>{data===''? '' : data.personalData.phoneNubmer}</p>
-            <p>{data===''? '' : data.personalData.email}</p>
+            <p>{data===''? '' : data.user.first_name}</p>
+            <p>{data===''? '' : data.user.middle_name}</p>
+            <p>{data===''? '' : data.user.last_name}</p>
+            <p>{data===''? '' : data.user.passport_num}</p>
+            <p>{data===''? '' : data.user.passport_num}</p>
+            <p>{data===''? '' : data.user.passport_issued_date}</p>
+            <p>{data===''? '' : data.user.passport_issued_by}</p>
+            <p>{data===''? '' : data.user.mobile_num}</p>
+            <p>{data===''? '' : data.user.email}</p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -68,7 +109,8 @@ const InsureApplicationModal = ({open, close, data}) => (
         positive
         icon="checkmark"
         labelPosition="right"
-        content="Accept"
+        content="Proof"
+        onClick={() => {handleOnProof(data.insureData.street, data.insureData.houseNumber)}}
       />
     </Modal.Actions>
   </Modal>
--- src/components/ListOfApplications/ListOfApplications.jsx
+++ src/components/ListOfApplications/ListOfApplications.jsx
@@ -1,8 +1,51 @@
 import React from "react";
-import { List, Segment, Header, Container } from "semantic-ui-react";
+import { List, Segment, Header, Container, Card, Button, Grid } from "semantic-ui-react";
 import InsureApplicationModal from './InsureApplication'
+import axios from 'axios'
 
 import './ListOfApplications.css'
+
+const tmp = [
+  {
+    "insurancerequestid": 4388924262558877900,
+    "propertytype": "home1",
+    "amount": 10000,
+    "policystartdate": "2019-04-16",
+    "policyenddate": "2019-04-16",
+    "policycreatedcate": "2019-04-16",
+    "status": "pending",
+    "address": {
+        "house_num": "1223",
+        "apartment_num": "string",
+        "street": "string",
+        "city": "string",
+        "state": "string",
+        "country": "string"
+    },
+    "user": {
+        "first_name": "Nethmi",
+        "middle_name": "Thileka",
+        "last_name": "Hettiarachchi",
+        "email": "a2222saa@gmafil.com22",
+        "mobile_num": "01311158f9654455",
+        "passport_num": "12w111sf3dErf",
+        "passport_issued_by": "Sri Lanka",
+        "passport_issued_date": "2019-04-13"
+    },
+    "company": {
+        "companyid": 1,
+        "companyname": "ABC Insurance",
+        "address": {
+            "house_num": "No12124",
+            "apartment_num": "Dorm3",
+            "street": "1, Universrity st",
+            "city": "Innopolis",
+            "state": "Tatarstan",
+            "country": "Russia"
+        }
+    }
+  },
+]
 
 // Tmp data
 const data = [
@@ -86,8 +129,33 @@ const data = [
 class ModalModalExample extends React.Component {
   state = {
     open: false,
-    data: data,
+    data: [],
     currentApp: ''
+  }
+
+  componentDidMount() {
+    if (this.state.data.length < 1) {
+      this.loadApplications()
+    }
+  }
+
+  loadApplications = () => {
+    let company_id = "1"
+    let status = "ALL"
+    let URL1 = `http://34.66.0.246:8080/api/V1/agents/requests?companyId=${company_id}&status=${status}`
+    let URL = 'http://127.0.0.1:5000/requests'
+    console.log(URL)
+    axios.get(URL)
+    .then(resp => {
+      this.setState({
+        data: resp.data.data
+      })
+      console.log(resp.data.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    console.log("done")
   }
 
   handleOnOpen = (app) => {
@@ -104,11 +172,50 @@ class ModalModalExample extends React.Component {
     })
   }
 
+  handleOnProof = () => {
+    const URL = 'http://127.0.0.1:5000/iroha_rest/api/v1.0/items'
+    // const URL2 = 'http://127.0.0.1:5000/iroha_rest/api/v1.0/items'
+    axios.post(URL, {
+        data: {
+          "item_type": "Kalatushkin",
+          "item_desc": "10"
+        }
+    }
+    )
+    .then(resp => {
+      console.log(resp)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    console.log('funciton proof')
+  }
+
   render() {
     return (
       <div>
         <Container>
           <Header as='h1' style={{paddingTop: '20px'}}>List of insurance applications</Header>
+          {/* <Grid>          
+          {this.state.data.map((app, index) => (
+            <Card>
+            <Card.Content>
+              <Card.Header style={{display: 'flex',justifyContent: 'space-between', alignItems: 'center'}}>
+                <div>Church Mutual Worker's Compensation Claim</div>
+                <div>#ID-1234567</div>
+              </Card.Header>
+              <Card.Meta style={{display: 'flex',justifyContent: 'space-between', alignItems: 'center'}}>
+                <p>
+                  Date Recieved: <span style={{color: 'red'}}>07/20/2018</span>
+                  <span style={{marginLeft: '30px'}}>Account Number: 76543210213</span>
+                </p>
+                <Button color="red" size="mini" content="Urgent" />
+              </Card.Meta>
+            </Card.Content>
+          </Card>
+          ))}
+          </Grid> */}
+          
           <Segment className="container">
           <List divided relaxed>
             {this.state.data.map((app, index) => (
@@ -116,17 +223,22 @@ class ModalModalExample extends React.Component {
               <List.Icon name="home" size="large" verticalAlign="middle" />
               <List.Content className="listItems">
                 <List.Header as="a" onClick={()=>this.handleOnOpen(app)}>
-                  Insurance contract number: {app.numberOfApp}
+                  Insurance contract number: {app.insurancerequestid}
                 </List.Header>
-                <List.Description as="a">Created: {app.insureData.dateStartOfAgreement}</List.Description>
+                <List.Description as="a">Created: {app.policycreatedcate}</List.Description>
               </List.Content>
             </List.Item>
             ))}
           </List>
           </Segment>
+         
         </Container>
         
-        <InsureApplicationModal data={this.state.currentApp} open={this.state.open} close={this.handleOnClose} />
+        <InsureApplicationModal 
+          data={this.state.currentApp} 
+          open={this.state.open} 
+          close={this.handleOnClose} 
+          handleOnProof={this.handleOnProof}/>
       </div>
     )
   }
